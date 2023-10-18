8cfb583d6488da71d996aa0f1f94e421d51764c1
aladvs <78510667+aladvs@users.noreply.github.com>
2023-10-18 00:23:55 -0500

    WORKING

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
+use std::default;
+
 use eframe::egui;
 use egui::*;
 //TODO :: MAKE SURE SCENES ARE PASSED ON FROM MAIN, SO EVERYTHING ACTUALLY WORKS
@@ -22,31 +24,46 @@ fn main() -> Result<(), eframe::Error> {
    // env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
     let options = eframe::NativeOptions::default();
         let mut content = Content {
-        text: String::new(),
+        text: String::default(),
+        current_scene: Scene::default()
     };
     eframe::run_native(
         "lad engine",
         options,
-        Box::new(|_cc| {
-            let my_scene = create_scene();
-            let my_app = App::from_scene(my_scene);
-            // or my_app = App { scene: my_scene, ... };
-            Box::new(my_app)
-          }
+        Box::new(|_cc| Box::<Content>::default()),
     )
 }
 
 #[derive(Default)]
 struct Content {
     text: String,
+    current_scene: Scene,
+}
+
+impl Default for Scene {
+    fn default() -> Self {
+        let dummy_mesh = Mesh {
+            vertices: vec![0.0, 0.0, 0.0],  // dummy vertex
+            indices: vec![0],  // dummy index
+            position: [0.0, 0.0, 0.0],
+            rotation: [0.0, 0.0, 0.0],
+            scale: [1.0, 1.0, 1.0],
+        };
+    
+        println!("Dummy mesh: {:?}", dummy_mesh);  
+    
+        Scene {
+            objects: vec![dummy_mesh], 
+        }
+    }
 }
 
 impl eframe::App for Content {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         let stroke = Stroke::new(2.0, Color32::WHITE);
         egui::CentralPanel::default().show(ctx, |ui| {
-            println!("Number of objects in scene: {}", current_scene.objects.len());  // Debug print
-            render_scene(current_scene, stroke);
+            println!("Number of objects in scene: {}", self.current_scene.objects.len()); 
+            render_scene(&self.current_scene, stroke);
             let line_start = Pos2::new(50.0, 50.0);
             let line_end = Pos2::new(800.0, 800.0);
             
@@ -59,17 +76,17 @@ fn initialize_scene() -> Scene {
     println!("Initializing scene...");
 
     let dummy_mesh = Mesh {
-        vertices: vec![0.0, 0.0, 0.0],  // A dummy vertex
-        indices: vec![0],  // A dummy index
+        vertices: vec![0.0, 0.0, 0.0],  // dummy vertex
+        indices: vec![0],  // dummy index
         position: [0.0, 0.0, 0.0],
         rotation: [0.0, 0.0, 0.0],
         scale: [1.0, 1.0, 1.0],
     };
 
-    println!("Dummy mesh: {:?}", dummy_mesh);  // Debug print
+    println!("Dummy mesh: {:?}", dummy_mesh);  
 
     Scene {
-        objects: vec![dummy_mesh],  // Adding the dummy mesh to the scene
+        objects: vec![dummy_mesh], 
     }
 }
 
@@ -78,15 +95,9 @@ fn initialize_scene() -> Scene {
 fn render_scene(scene: &Scene, stroke: Stroke) {
     println!("Rendering scene...");
 
-    println!("Length of objects: {}", scene.objects.len());  // Debug print
-
-    if scene.objects.is_empty() {
-        println!("No objects in the scene.");
-    } else {
-        println!("Number of objects: {}", scene.objects.len());
+    println!("Length of objects: {}", scene.objects.len());  
 
         for (i, mesh) in scene.objects.iter().enumerate() {
             println!("Mesh {}: {:?}", i, mesh);
         }
     }
-}
