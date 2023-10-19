eb853562681107704ac13fc9f1a5ecb60c98a53e
aladvs <78510667+aladvs@users.noreply.github.com>
2023-10-19 00:36:42 -0500

    draw

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@
 use eframe::egui;
 use egui::*;
 use tmf::TMFMesh;
-use std::fs::File;
 
 //TODO :: MAKE SURE SCENES ARE PASSED ON FROM MAIN, SO EVERYTHING ACTUALLY WORKS
 struct Scene {
@@ -13,7 +12,7 @@ struct Scene {
 #[derive(Debug)] 
 struct Mesh {
     vertices: Vec<(f32, f32, f32)>,
-    indices: Vec<i32>,
+    indices: Vec<u32>,
     position: [f32; 3],
     rotation: [f32; 3], 
     scale: [f32; 3],
@@ -29,10 +28,7 @@ fn main() -> Result<(), eframe::Error> {
     icon_data: Some(load_icon()),
     ..Default::default()
 };
-        let mut content = Content {
-        text: String::default(),
-        current_scene: Scene::default()
-    };
+
     eframe::run_native(
         "lad engine",
         options,
@@ -48,7 +44,7 @@ struct Content {
 
 impl Default for Scene {
     fn default() -> Self {
-        let mut input = include_bytes!("suzanne.obj");
+        let mut input = include_bytes!("sphere.obj");
         let mut input_slice: &[u8] = input;
         let (mesh, name) = TMFMesh::read_from_obj_one(&mut input_slice).expect("Could not read TMF file!");
         let obj_vertices = mesh.get_vertices().expect("No vertices!");
@@ -57,9 +53,12 @@ impl Default for Scene {
     .iter()
     .map(|&v| (v.0, v.1, v.2))
     .collect();
+        let vertex_triangles = mesh.get_vertex_triangles().expect("No vertiex triangle array!");
+        //let indices: Vec<u32> = vertex_triangles.to_vec();
+
         let dummy_mesh = Mesh {
             vertices: vertices.to_vec(),  // dummy vertex
-            indices: vec![0],  // dummy index
+            indices: [0, 0].to_vec(),  // dummy index
             position: [0.0, 0.0, 0.0],
             rotation: [0.0, 0.0, 0.0],
             scale: [1.0, 1.0, 1.0],
@@ -75,30 +74,49 @@ impl Default for Scene {
 
 impl eframe::App for Content {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        let mut deltaTime = ctx.input(|ctx| ctx.predicted_dt);
+      //  println!("{}",deltaTime);
         let stroke = Stroke::new(2.0, Color32::WHITE);
         egui::CentralPanel::default().show(ctx, |ui| {
-            println!("Number of objects in scene: {}", self.current_scene.objects.len()); 
+            //println!("Number of objects in scene: {}", self.current_scene.objects.len()); 
             render_scene(&self.current_scene, stroke, &ui);
         });
+        ctx.request_repaint();
     }
 }
 
 
 
-
 fn render_scene(scene: &Scene, stroke: Stroke, ui: &Ui) {
-    println!("Rendering scene...");
+    let canvas_width = ui.ctx().screen_rect().width();
+    let canvas_height = ui.ctx().screen_rect().height();
+    let half_width = canvas_width / 2.0;
+    let half_height = canvas_height / 2.0;
 
-    println!("Length of objects: {}", scene.objects.len());  
 
-        for (i, mesh) in scene.objects.iter().enumerate() {
-            println!("Mesh {}: {:?}", i, mesh);
-            let line_start = Pos2::new(50.0, 50.0);
-            let line_end = Pos2::new(800.0, 800.0);
-            
-            ui.painter().line_segment([line_start, line_end], stroke);
+    let mut painter = ui.painter();
+
+    for mesh in &scene.objects {
+        let vertices = &mesh.vertices;
+
+        for (i, start) in vertices.iter().enumerate() {
+            for end in &vertices[i + 1..] {
+                let line_start = Pos2::new(
+                    start.0*100.0 + half_width,
+                    start.1*100.0 + half_height,
+                );
+                let line_end = Pos2::new(
+                    end.0*100.0 + half_width,
+                    end.1*100.0 + half_height,
+                );
+
+                // Use the stroke method to draw the line segment
+                painter.line_segment([line_start, line_end], stroke);
+            }
         }
     }
+}
+
 
     pub(crate) fn load_icon() -> eframe::IconData {
         let (icon_rgba, icon_width, icon_height) = {
